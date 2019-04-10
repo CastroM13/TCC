e00354f5f5bd03f2189a2b9279118362490a8160
CastroM13 <49277207+CastroM13@users.noreply.github.com>
2019-04-10 18:03:41 -0300

    GOGO

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 function login(){
     var rm = document.getElementById("rm")
     var pwd = document.getElementById("pwd")
-    if(rm.value == 1 && pwd.value == 2){
-        window.location.href = "band.html";
+    if(rm.value == "" && pwd.value == ""){
+        document.getElementById("err").innerHTML = "Preencha os campos!" 
+    }else{
+    if(rm.value == "160182" && pwd.value == "5398"){
+        window.location.href = "main.html";
         return false;
     } else{
         document.getElementById("err").innerHTML = "Login Inválido"
     }
 }
+}
 
-function clicado(){
+function open(){
     alert("Fui clicado!")
 }
